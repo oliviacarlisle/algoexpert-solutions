edce512e0523fd537a913a148ddc8efda1718718
Olivia Carlisle <151247171+oliviacarlisle@users.noreply.github.com>
2023-12-30 11:24:27 -0500

    Modify Zero Sum Subarray solution using Set

--- src/zeroSumSubarray.js
+++ src/zeroSumSubarray.js
@@ -1,11 +1,11 @@
 function zeroSumSubarray(nums) {
-    const sums = new Map();
+    const sums = new Set();
     let sum = 0;
     for (let i = 0; i < nums.length; i++) {
       sum += nums[i];
       if (sum === 0) return true;
       else if (sums.has(sum)) return true;
-      else sums.set(sum, true);
+      else sums.add(sum);
     }
     return false;
 }
