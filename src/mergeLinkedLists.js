609dc3fa7a404504a9bbf7627dfe9ec0e2980970
Olivia Carlisle <151247171+oliviacarlisle@users.noreply.github.com>
2024-01-14 22:33:58 -0500

    Solve Merge Linked Lists

---
+++ src/mergeLinkedLists.js
@@ -0,0 +1,28 @@
+class LinkedList {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+function mergeLinkedLists(headOne, headTwo) {
+
+  let p1 = headOne;
+  let p1Prev;
+  let p2 = headTwo;
+  while (p1 && p2) {
+    
+    if (p1.value < p2.value) {
+      p1Prev = p1;
+      p1 = p1.next;
+    } else {
+      if (p1Prev) p1Prev.next = p2;
+      p1Prev = p2;
+      p2 = p2.next;
+      p1Prev.next = p1;
+    } 
+  }
+  if (!p1) p1Prev.next = p2;
+
+  return (headOne.value < headTwo.value) ? headOne : headTwo;
+}
